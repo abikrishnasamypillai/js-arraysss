0320235f3d7979768033d6b4df4cf2e87b894f72
abikrishnasamypillai <abirammykarthika@gmail.com>
2020-03-05 17:43:37 +0530

    1 file modified

---
+++ index.js
@@ -0,0 +1,40 @@
+//Array methods
+
+
+//forEach
+/*
+array=['Abi','Amri','Lavanya','Kavya'];
+
+array.forEach(
+  data=console.log(data)
+)*/
+
+
+//concat
+//concat is used to concatenate two arryas into array
+let array=["abi","amri"];
+let array2=["lavanya","venu"];
+let totalArray=array.concat(array2);
+console.log(totalArray);
+//output :["abi", "amri", "lavanya", "venu"]
+
+//filter
+
+var studentAge = [21,20,14,16,18,18,19].filter(ageVal)
+function ageVal(value) {
+  return value >=18;
+}
+console.log(studentAge);
+
+//include
+//checks whether the array contains the element
+var alphabets=['a','e','i','o','u']
+let value=alphabets.includes('a')
+console.log(value);
+
+//slice
+
+var slice=('a-apple','b-ball','c-cat','d-dog','e-elephant')
+let 
+
+
